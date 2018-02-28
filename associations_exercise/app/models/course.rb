55167b64b344257944c7d807a11f9914609bd671
Samuel Chia <cheeeya@berkeley.edu>
2018-02-28 15:14:27 -0800

    Finish associations exercise

--- associations_exercise/app/models/course.rb
+++ associations_exercise/app/models/course.rb
@@ -1,2 +1,33 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :integer          not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Course < ApplicationRecord
+
+  has_many :enrollments,
+    class_name: :Enrollment,
+    primary_key: :id,
+    foreign_key: :course_id
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+  has_one :prerequisite,
+    foreign_key: :prereq_id,
+    primary_key: :id,
+    class_name: :Course
+
+  has_one :instructor,
+    primary_key: :instructor_id,
+    foreign_key: :id,
+    class_name: :User
 end
